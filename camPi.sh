7e4df6eb2af2bc8fafd925e39fff01e6cc942cc9
Florian <florian.rau@mdc-berlin.de>
2019-07-19 15:07:14 +0100

    ...

--- camPi.sh
+++ camPi.sh
@@ -1,49 +1,89 @@
 #!/bin/bash
 
 # Define IP addresses
-IP1=10.0.0.10
-IP2=10.0.0.11
-IP3=10.0.0.12
-IP4=10.0.0.13
-
-# Define Port
-PORT=5000
+IPS[0]=10.0.0.10
+IPS[1]=10.0.0.11
+IPS[2]=10.0.0.12
+IPS[3]=10.0.0.13
 
 # Define labels
-LABEL1="SETUP 0"
-LABEL2="SETUP 1"
-LABEL3="SETUP 2"
-LABEL4="SETUP 3"
+LABELS[0]="SETUP 0"
+LABELS[1]="SETUP 1"
+LABELS[2]="SETUP 2"
+LABELS[3]="SETUP 3"
+
+# Define parameters for remote login
+REMOTEUSER=pi
+REMOTEPASS=raspberry
 
 
+# DO NOT CHANGE ANYTHING BELOW THIS LINE --------------------------------------
 
-# DO NOT CHANGE ANYTHING BELOW THIS LINE! ==========================
 
 # Get screen resolution
 resX=`fbset -s | grep -Po 'mode "\K[^x]*'`
 resY=`fbset -s | grep -Po 'mode "[[:digit:]]*x\K[^"]*'`
 
-# Start cameras
-COMMON="killall -q -w raspivid; raspivid -t 0 -n -ih -w $((resX/2)) -h $((resY/2)) -l -o tcp://0.0.0.0:$PORT"
-ssh pi@$IP1 "$COMMON -a '$LABEL1'" &
-ssh pi@$IP2 "$COMMON -a '$LABEL2'" &
-ssh pi@$IP3 "$COMMON -a '$LABEL3'" &
-ssh pi@$IP4 "$COMMON -a '$LABEL4'" &
-sleep 5
-
 # Define windows
-WIN1="0,0,$((resX/2)),$((resY/2))"
-WIN2="$((resX/2)),0,$resX,$((resY/2))"
-WIN3="0,$((resY/2)),$((resX/2)),$resY"
-WIN4="$((resX/2)),$((resY/2)),$resX,$resY"
+WIN[0]="0,0,$((resX/2)),$((resY/2))"
+WIN[1]="$((resX/2)),0,$resX,$((resY/2))"
+WIN[2]="0,$((resY/2)),$((resX/2)),$resY"
+WIN[3]="$((resX/2)),$((resY/2)),$resX,$resY"
+
+# Wait for network
+echo "Waiting for network ..."
+while ! timeout 0.2 ping -c 1 `hostname -I` &> /dev/null; do
+	sleep 0.2
+done
+
+# Wait for cameras to appear on the network
+for i in "${!IPS[@]}"; do
+	printf "%s" "Looking for ${IPS[$i]} (${LABELS[$i]}) ..."
+	EXIST["$i"]=0
+	for j in {1..50}; do
+		if timeout 0.2 ping -c 1 -n "${IPS[$i]}" &> /dev/null; then
+			echo " Found"
+			EXIST["$i"]=1
+			break
+		else
+			printf "%c" "."
+		fi
+	done
+	if [[ "${EXIST[$i]}" -eq 0 ]]; then
+		unset "IPS[$i]"
+		echo " Not found - giving up"
+	fi
+done
+
+# Killing existing processes
+printf "%s" "Killing existing streams ..."
+killall -q screen
+for IP in "${IPS[@]}"; do
+	sshpass -p$REMOTEPASS ssh $REMOTEUSER@$IP "killall -q -w raspivid" &
+done
+wait
+echo " Done"
 
 # Start streams
+PORT=5000
+COMMON="raspivid -t 0 -n -ih -sa -100 -w $((resX/2)) -h $((resY/2)) -l -o tcp://0.0.0.0:$PORT"
+for i in "${!IPS[@]}"; do
+	echo "Starting stream at ${IPS[$i]} (${LABELS[$i]}) ..."
+	sshpass -p$REMOTEPASS ssh $REMOTEUSER@${IPS[$i]} "$COMMON -a '${LABELS[$i]}' &>/dev/null" &
+done
+sleep 0.5
+
+# Watch streams
+echo "Connecting to streams ..."
 COMMON="--video_fifo 0 --video_queue 0.2"
-screen -dmS stream1 sh -c "omxplayer --win $WIN1 $COMMON tcp://$IP1:$PORT"
-screen -dmS stream2 sh -c "omxplayer --win $WIN2 $COMMON tcp://$IP2:$PORT"
-screen -dmS stream3 sh -c "omxplayer --win $WIN3 $COMMON tcp://$IP3:$PORT"
-screen -dmS stream4 sh -c "omxplayer --win $WIN4 $COMMON tcp://$IP4:$PORT"
+for i in "${!IPS[@]}"; do
+	screen -dmS stream$i sh -c \
+		"omxplayer --win ${WIN[$i]} $COMMON tcp://${IPS[$i]}:$PORT" &
+done
 
 # Wait for CTRL+C
 trap '{ killall -q screen; echo ""; exit 1; }' INT
 sleep infinity
+
+
+
